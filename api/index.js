43ca32f94d9857c4f5e2357f9dcea6acb2f24d75
zakjak <78931393+zakjak@users.noreply.github.com>
2024-02-23 02:17:14 -0600

    Done with error handlers for the backend

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from 'cors'
 import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import authRouter from './routes/auth.route.js'
 
 const app = express()
 
@@ -13,9 +15,22 @@ app.use(cors({
 dotenv.config()
 
 // DATABASE CONNECTION
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log('Database Connected'))
 
+app.use('/api/auth', authRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 })
---
+++ api/routes/auth.route.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const router = express.Router()
+
+router.post('/auth', )
+
+export default router
