946010eceef7d8fa0279f88f1efc1fe7d7f5b195
zakjak <78931393+zakjak@users.noreply.github.com>
2024-02-26 16:13:56 -0600

    implement toaster for users who are not signed in to prevent them from commenting or liking post

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom'
 import { FaHeart, FaPlay } from "react-icons/fa";
 import EmojiPicker from 'emoji-picker-react'
 import { useSelector } from 'react-redux';
+import { Toast } from 'flowbite-react'; 
 
 function Post() {
     const { currentUser } = useSelector(state => state.user)
@@ -11,6 +12,8 @@ function Post() {
     const [user, setUser] = useState([])
     const [comments, setComments] = useState([])
     const [comment, setComment] = useState('')
+    const [showLikeToast, setShowLikeToast] = useState(false)
+    const [showCommentToast, setShowCommentToast] = useState(false)
     const [emoji, setEmoji] = useState(false)
 
     const postId = useParams()
@@ -22,7 +25,7 @@ function Post() {
     }, [postId, posts[0]?.owner])
     
     const getPost = async () => {
-        const res = await fetch(`/api/post/getPost?postId=${postId.id}`)
+        const res = await fetch(`/api/post/getPost?postId=${postId?.id}`)
         const data = await res.json()
     
         if(res.ok){
@@ -32,7 +35,7 @@ function Post() {
 
       const getUser = async () =>{
         try{
-          const res = await fetch(`/api/user/getUser/${posts[0].owner}`)
+          const res = await fetch(`/api/user/getUser/${posts[0]?.owner}`)
     
           if(res.ok){
             const data = await res.json()
@@ -62,19 +65,25 @@ function Post() {
       const handleSubmit = async(e) => {
         e.preventDefault()
         try{
-            const res = await fetch('/api/comment/createComment', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    comment,
-                    postId: postId.id,
-                    owner: user?._id
+            if(currentUser){
+                const res = await fetch('/api/comment/createComment', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        comment,
+                        postId: postId.id,
+                        owner: user?._id
+                    })
                 })
-            })
-            if(res.ok){
-                const data = await res.json()
-                getComment()
-                setComment('')
+
+                if(res.ok){
+                    const data = await res.json()
+                    getComment()
+                    setComment('')
+                }
+            }
+            if(!currentUser){
+                setShowCommentToast(true)
             }
         }catch(err){
             console.log(err)
@@ -83,12 +92,20 @@ function Post() {
 
       const likeComment = async (commentId) => {
         try{
-            const res = await fetch(`/api/comment/likeComment/${commentId}/${currentUser._id}`, {
-                method: 'PUT',
-                headers: {'Content-Type': 'application/json'},
-            })
-            if(res.ok){
-                getComment()
+            if(currentUser){
+                const res = await fetch(`/api/comment/likeComment/${commentId}/${currentUser?._id}`, {
+                    method: 'PUT',
+                    headers: {'Content-Type': 'application/json'},
+                })
+
+                if(res.ok){
+                    getComment()
+                }
+            }
+
+            if(!currentUser){
+                setShowLikeToast(true)
+                return;
             }
         }catch(err){
             console.log(err)
@@ -125,18 +142,33 @@ function Post() {
                                                 <div className="flex items-center gap-4">
                                                     <div className="text-xl flex items-center gap-1">
                                                         <FaHeart 
+                                                            title='like'
                                                             className={`cursor-pointer ${comment.likes.includes(currentUser?._id) ? 'text-red-900' : 'text-white'}`} 
                                                             onClick={() => likeComment(comment?._id)} />
                                                         <span className='text-sm'>{`${comment?.numberOfLikes > 0 ? comment?.numberOfLikes : ''}`}</span>
                                                     </div>
-                                                    <div className="text-sm">Reply</div>
+                                                    <div className="text-sm cursor-pointer" title='reply'>Reply</div>
                                                 </div>
+                                                {
+                                                    showLikeToast && (
+                                                        <Toast>
+                                                            <div className="ml-3 text-sm font-normal">Login to Like Comment</div>
+                                                            <Toast.Toggle onDismiss={() => setShowLikeToast(false)} />
+                                                        </Toast>
+                                                    )
+                                                }
                                             </div>
                                         ))
                                     }
                                 </div>
                             </div>
                             <div className="mt-2">
+                            {showCommentToast && (
+                                            <Toast className='absolute bottom-32'>
+                                                <div className="ml-3 text-sm font-normal">Login to Comment</div>
+                                                <Toast.Toggle onDismiss={() => setShowCommentToast(false)} />
+                                            </Toast>
+                                        )}
                                 <div className="flex items-center border rounded-full h-12 px-2">
                                     <form onSubmit={handleSubmit} className='flex justify-center items-center w-full'>
                                         <input value={comment} onChange={e => setComment(e.target.value)} className='max-h-12 outline-none flex-1 bg-transparent' />
